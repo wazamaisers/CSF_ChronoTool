69ed2cd0a7c5b8d5ed30c05d4b96fdce20bb61e5
Tiago Martins <tiago.mousinho.martins@gmail.com>
2016-11-12 19:22:01 +0000

    Drive refatorizada ms não acabada + Skype entrys

--- Chrono Tool/src/ChronoTool.java
+++ Chrono Tool/src/ChronoTool.java
@@ -2,53 +2,68 @@ import java.sql.*;
 import java.util.*;
 
 import Drive.*;
+import Drive.Database.DatabaseSnapshotEntry;
 
 public class ChronoTool
 {
 
 	public static void main( String args[] ) throws SQLException
 	{
-		
+
 		// Simulação de username
 		String username = "Tiago";
-		
+
 		// Importação da base de dados do ficheiro BD para o programa
-		Database database = new Database(username);
-		HashMap <String, ArrayList<DatabaseEntry>> database_schema = database.getDatabaseSchema();
-		ArrayList<DatabaseEntry> database_entrys = database.getDatabaseEntrys();
-		DatabaseAnalisis database_analisis = new DatabaseAnalisis();
-		
+		Drive drive = new Drive(username);
+
 		// Detalhes do útilizador
-		new UserDetails(username);
-		System.out.println("");
-		
+		//new UserDetails(username);
+		//System.out.println("");
+
 		// Percentagem de ficheiros modificados pela última vez a cada altura do dia
 		Timestamps tmstp = new Timestamps(username);
 		ArrayList<Integer> lista = tmstp.getTimestamps();
 		tmstp.getTimes(lista);
 		System.out.println("");
-		
+
 		// Ver o nome dos ficheiros e/ou pastas numa determinada pasta
-		
-		System.out.println("Ficheiros dentro da pasta AR");
-		String path = "root/AR";
-		String doc_id = database_analisis.getDirectoryDocIdByPath(path, database_entrys);
-		
-		for(DatabaseEntry entry: database_schema.get(doc_id)){
-			System.out.println(entry.getFilename());
-		}
-		System.out.println("");
-		
+
+		String path = "root/CSF";
+		System.out.println("Ficheiros dentro da pasta " + path); //Esta funcao tem de se definir na drive
+		String doc_id = drive.getDirectoryDocIdByPath(path);     //Para usar o for debaixo la
+
+		//for(DatabaseEntry entry: database_schema.get(doc_id)){
+		//	System.out.println("Type: " + entry.getResourceType() + " -> Name: " + entry.getFilename());
+		//}
+		//System.out.println("");
+
 		// Ver as extensões de todos os ficheiros que se encontram numa dada pasta e suas filhas
 		
-		System.out.println("Extensão de todos os ficheiros dentro da pasta Tiago, incluindo em pastas filhas");
-		path = "root/Tiago";
-		doc_id = database_analisis.getDirectoryDocIdByPath(path, database_entrys);
-		ArrayList<DatabaseEntry> children = database_analisis.getArrayOfChildren(doc_id, database_schema);
-		HashMap <String, Integer> hm = database_analisis.getExtensions(children);
-		
+		System.out.println("Extensão de todos os ficheiros dentro da pasta " + path);
+		HashMap <String, Integer> hm = drive.getExtensions(path);
+
 		for(HashMap.Entry<String, Integer> entry: hm.entrySet()){
-			System.out.println("Extensão : " + entry.getKey() + " Quantidade: " + entry.getValue());
+			System.out.println("Extension: ." + entry.getKey() + " -> Quantity: " + entry.getValue());
 		}
+		System.out.println("");
+
+		// Ver as extensões de todos os ficheiros que se encontram numa dada pasta e suas filhas
+
+		System.out.println("Extensão de todos os ficheiros dentro da pasta " + path + ", incluindo em pastas filhas");
+		hm = drive.getChildrenExtensions(path);
+
+		for(HashMap.Entry<String, Integer> entry: hm.entrySet()){
+			System.out.println("Extension: ." + entry.getKey() + " -> Quantity: " + entry.getValue());
+		}
+		
+		
+		
+		
+		
+		//////////////////////////////////////////////////////////////////////////////////////////////
+		//DROPBOX
+		
+		
+		
 	}
 }
